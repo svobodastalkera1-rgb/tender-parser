5023547f173a11905601c65cc7a062ece01b3d00
cunwass <svobodastalkera1@gmail.com>
2026-09-13 01:17:35 +0500

    add functions to read from and write to the database

--- database.py
+++ database.py
@@ -1,20 +1,73 @@
 import os
 import psycopg
 from dotenv import load_dotenv
+import dns
 
 load_dotenv()
 
-with psycopg.connect(
-    dbname = os.getenv('DB_NAME'),
-    user = os.getenv('DB_USER'),
-    password = os.getenv('DB_PASSWORD'),
-    host = os.getenv('DB_HOST'),
-    port = os.getenv('DB_PORT')
-) as connection:
+def get_connection():
+    return psycopg.connect(
+        dbname = os.getenv('DB_NAME'),
+        user = os.getenv('DB_USER'),
+        password = os.getenv('DB_PASSWORD'),
+        host = os.getenv('DB_HOST'),
+        port = os.getenv('DB_PORT')
+    )
 
-    with connection.cursor() as cursor:
+def save_data(results):
 
-        pass
+    query = """ INSERT INTO items(
+                        title,
+                        price,
+                        currency,
+                        availability,
+                        partnumber,
+                        url,
+                        article,
+                        specs,
+                        title_bd,
+                        shop
+                    ) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                        
+                    ON CONFLICT (partnumber, article) DO UPDATE SET 
+                        price = EXCLUDED.price,
+                        availability = EXCLUDED.availability,
+                        shop = EXCLUDED.shop,
+                        url = EXCLUDED.url; """
+
+    with get_connection() as connection:
+        with connection.cursor() as cursor:
+
+            for result in results:
+                
+                values = (
+                    getattr(result, 'title', None),
+                    getattr(result, 'price', None),
+                    getattr(result, 'currency', 'RUB'),
+                    getattr(result, 'availability', None),
+                    getattr(result, 'partnumber', None),
+                    getattr(result, 'url', None),
+                    getattr(result, 'article', None),
+                    getattr(result, 'specs', None),
+                    getattr(result, 'title_bd', None),
+                    getattr(resilt, 'shop', None)
+                )
+
+                cursor.execute(query, values)
+
+            connection.commit()
+
+def get_item(partnumber, title):
+
+    query = """ SELECT title, price, shop, availability FROM items WHERE partnumber = %s OR title = %s; """ 
+
+    with get_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(query, (partnumber, title))
+            return cursor.fetchall()
+
+
+
 
 
 
