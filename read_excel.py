8947761d319c39f56c46c035dd4992dd3dff9716
cunwass <svobodastalkera1@gmail.com>
2026-09-13 23:35:21 +0500

    Refactoring read_tender_excel function (remove junk, simplify logic, clear code)

--- read_excel.py
+++ read_excel.py
@@ -1,10 +1,13 @@
 import sys                              #модуль для работы с аргументами командной строки
-from openpyxl import load_workbook      #функция для чтения Excel файлов
+from openpyxl import load_workbook     #функция для чтения Excel файлов
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+file_path = os.getenv("LOCAL_EXCEL") 
 
-#функция для чтения excel
 def read_tender_excel(file_path):       #при вызове функции передается путь к файлу Excel
-                                        #data_only=True = получение значение формулы, если она пристуствует в ячейке
+                           
     workbook = load_workbook(file_path, data_only=True)
     sheet = workbook.active             #получение активного листа Excel файла
 
@@ -12,30 +15,11 @@ def read_tender_excel(file_path):       #при вызове функции пе
     for row in sheet.iter_rows(min_row=2, values_only=True): #min_row=2 = начало чтения со второй строки, va;ues_omly=True = получение значения, а не объекта ячеек
         if row[0] and row[1]:
             name = str(row[0]).strip()
-            quantity = row[1]
-            
-            if row[2]:
-                unit = str(row[2]).strip()
-            else:
-                unit = "шт"
 
             #составляем словарь с данными по позиции
             items.append({
-                'name': name,
-                'quantity': quantity,
-                'unit': unit
+                'name': name
             })
     return items
 
-
-#этот код' будет выполнен только при запуске файла напрямую
-if __name__ == "__main__":      #__name__ = переменная, созданая питоном самостоятельно и автомоатически
-    if len(sys.argv) != 2:      #sys.argv = список аргументов командной строки, через len узнаем их количество 
-        print('Используй только: python read_excel.py "путь к файлу"')
-        sys.exit(1)
-
-file_path = sys.argv[1]                 #получаем путь к файлу из аргументов
-data = read_tender_excel(file_path)     #вызываем функцию и получаем список позиций
-
-for item in data:                       #печатаем каждую позицию
-    print (f"{item['name']} | {item['quantity']} | {item['unit']}")
+#data = read_tender_excel(file_path)     #вызываем функцию и получаем список позиций
