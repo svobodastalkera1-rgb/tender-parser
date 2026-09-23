4627afe6eb4e90bdf523c5f33702b6df4e6845e6
cunwass <svobodastalkera1@gmail.com>
2026-09-23 23:26:34 +0500

    Improve Excel parsing function for better data extraction

--- read_excel.py
+++ read_excel.py
@@ -7,11 +7,22 @@ def read_tender_excel(file_path):       #при вызове функции пе
 
     items = []
     for row in sheet.iter_rows(min_row=2, values_only=True): #min_row=2 = начало чтения со второй строки, va;ues_omly=True = получение значения, а не объекта ячеек
-        if row[0] and row[1]:
-            name = str(row[0]).strip()
-
-            #составляем словарь с данными по позиции
+        if row[0] is None and row[1]:
+            name = str(row[1]).strip()
             items.append({
-                'name': name
+                name
+            })            
+
+        if row[0] and row[1] is None:
+            name = str(row[0]).strip()
+            items.append({
+                name
+            })
+
+        if row[0] and row[1]:
+            name = str(row[0]).strip() + ' ' + str(row[1]).strip()
+
+            items.append({
+                name
             })
     return items
