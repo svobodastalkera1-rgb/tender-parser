81c8114a59d5ea0fb5ae61a831eba13e9f92f62b
cunwass <svobodastalkera1@gmail.com>
2026-09-25 00:02:52 +0500

    minor changes, improve working

--- read_excel.py
+++ read_excel.py
@@ -9,20 +9,20 @@ def read_tender_excel(file_path):       #при вызове функции пе
     for row in sheet.iter_rows(min_row=2, values_only=True): #min_row=2 = начало чтения со второй строки, va;ues_omly=True = получение значения, а не объекта ячеек
         if row[0] is None and row[1]:
             name = str(row[1]).strip()
-            items.append({
+            items.append(
                 name
-            })            
+            )            
 
         if row[0] and row[1] is None:
             name = str(row[0]).strip()
-            items.append({
+            items.append(
                 name
-            })
+            )
 
         if row[0] and row[1]:
             name = str(row[0]).strip() + ' ' + str(row[1]).strip()
 
-            items.append({
+            items.append(
                 name
-            })
+            )
     return items
